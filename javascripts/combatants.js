670ee73a2f7c1e38b8a2037e7ebc4f8b2de472b8
juliakimchung <julhkm@gmail.com>
2016-11-11 21:04:11 -0600

    made some changes for your review

--- javascripts/combatants.js
+++ javascripts/combatants.js
@@ -9,9 +9,9 @@ let Player = function() {
 	this.currentWeapon = null;
 };
 
-Player.prototype.setWeapons = function(items){
-	let newWeapons = this.weapons.concat(items);
-	return newWeapons;
+Player.prototype.setWeapons = function(weapons){
+	this.weapons = weapons? this.weapons.concat(weapons) : this.weapons
+	
 };
 
 Player.prototype.getCurrentWeapon = function(){
@@ -22,16 +22,17 @@ Player.prototype.getCurrentWeapon = function(){
 let Cop = function(){
 	this.class = "cop";
 	this.health = 20;
-	this.weapons = Cop.setWeapons(["shotgun", "nightstick"]);
+	this.setWeapons(["shotgun", "nightstick"]);
 };
 Cop.prototype = new Player();
 
 let Criminal = function(){
 	this.class = "criminal";
 	this.health = 20;
-	this.weapons = Criminal.setWeapons(["uzi", "butterfly_knive"]);
+	this.setWeapons(["uzi", "butterfly_knive"]);
 };
 Criminal.prototype = new Player();
+console.log(new Player);
 
 let weapons = function(){
 	this.damage = null;
@@ -40,19 +41,22 @@ let weapons = function(){
 let shotgun = function(){
 	this.damage = 5;
 };
+	shotgun.prototype = new weapons();
 
 let nightstick = function(){
 	this.damage = 3;
 };
-
+  nightstick.prototype = new weapons();
 
 let uzi = function(){
 	this.damage = 6;
 };
-
+	uzi.prototype = new weapons();
 let butterfly_knive = function(){
 	this.damage = 2;
 };
+butterfly_knive.prototype = new weapons();
 
- module.exports = { Cop, Criminal , weapons, shotgun, nightstick, uzi, butterfly_knive};
 
+
+ module.exports = { Cop,Criminal, weapons, shotgun, nightstick, uzi, butterfly_knive};
