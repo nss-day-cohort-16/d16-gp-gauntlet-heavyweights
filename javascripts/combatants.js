2a96dccc736f019bbf7a49ffcf32753afd0538e1
juliakimchung <julhkm@gmail.com>
2016-11-14 08:15:58 -0600

    made changes  on this.weapon=[]

--- javascripts/combatants.js
+++ javascripts/combatants.js
@@ -9,25 +9,27 @@ let Player = function(name){
 	this.health = ' ';
 	this.weapons = [];
 	this.damage = ' ';
-	this.currentWeapon = ' ';
+	this.currentWeapon = [];
 };
 
 Player.prototype.setWeapons = function(weapons){
 	console.log('setWeapons is running');
-	this.weapons = weapons ? this.weapons.concat(weapons, ' ') : this.weapons;
+	this.weapons = weapons ? this.weapons.concat(weapons) : this.weapons;
 	console.log("weapon", weapons );
 };
 
-Player.prototype.getCurrentWeapon = function(){
+Player.prototype.getCurrentWeapon = function(weapon){
 	let currentWeapon = this.weapons[Math.floor(Math.random() * (this.weapons.length - 1))];
 	console.log("currentWeapon", currentWeapon);
 	return currentWeapon;
 };
 
+
 let Cop = function(){
 	this.class = "cop";
 	this.health = 20;
-	this.setWeapons(["shotgun", "nightstick"]);
+	this.setWeapons(["shotgun", " nightstick"]);
+	this.getCurrentWeapon();
 	console.log('new Cop', this);
 };
 Cop.prototype = new Player();
@@ -35,7 +37,8 @@ Cop.prototype = new Player();
 let Criminal = function(){
 	this.class = "criminal";
 	this.health = 20;
-	this.setWeapons(["uzi", "butterfly_knife"]);
+	this.setWeapons(["uzi", " butterfly_knife"]);
+	this.getCurrentWeapon();
 	console.log('new Criminal', this);
 
 };
