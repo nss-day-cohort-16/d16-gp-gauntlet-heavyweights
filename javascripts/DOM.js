4e89a856fe1aac0fb9ef54c00ea20a4983cd06d9
juliakimchung <julhkm@gmail.com>
2016-11-14 08:12:25 -0600

    updated DOM event

--- javascripts/DOM.js
+++ javascripts/DOM.js
@@ -3,7 +3,7 @@ let gamegen = require("./gamegen");
 let combatants = require("./combatants");
 let weapons = require("./weapons");
 
- 
+
 
  $("#userInput").keyup(function(e) {
  	let code = e.which;
@@ -13,17 +13,18 @@ let weapons = require("./weapons");
  		console.log("capturePlayerName", playerName); 		
  	}
  });
-
-$(".playerClass").click(function(event){
-	if(event.target.id === 'cop'){
-		Gauntlet.setPlayerClass('cop');
-	}else if(event.target.id === 'criminal'){
-		Gauntlet.setPlayerClass('criminal');
+let cop = document.getElementById('cop');
+let criminal = document.getElementById('criminal');
+$(".playerClass").click(function(){
+	if(event.target.id === "cop"){
+		gamegen.Gauntlet.setPlayerClass(cop);
+	}else if(event.target.id === "criminal"){
+		gamegen.Gauntlet.setPlayerClass(criminal);
 	}
 });
 
- 
- 
+ // Gauntlet.playerAttack();
+
  $("#attack").click(function(e){
  	console.log("clicked Attack");
  	
@@ -31,3 +32,34 @@ $(".playerClass").click(function(event){
  	
  });
 
+$(document).ready(function() {
+
+
+ let copInfo = $("#copInfo");
+ let criminalInfo = $("#criminalInfo");
+
+
+ $("#attack").on("click", function(e) {
+ 	gamegen.Gauntlet.setPlayerClass();
+ 	gamegen.Gauntlet.playerAttack();
+ 	copInfo.html(
+ 		`<p>health: ${combatants.Cop.health}</p>
+ 		 <img src = "">`);
+ 	criminalInfo.html(
+ 		 `<p>health: ${combatants.Criminal.health}</p>
+ 		  <img src = "">`);
+
+ 	let checkHealth = function(newPlayerHealth, newEnemyHealth){
+	if(newPlayerHealth === 0){
+		console.log(`${combatants.Player.name} is dead!`);
+	}else if(newEnemyHealth === 0){
+		console.log(`...is dead!`);
+	}else{
+		console.log('Keep fighting!!!!');
+	}
+};
+
+
+ });
+
+});
